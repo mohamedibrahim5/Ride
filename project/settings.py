db65c0cac1aaada8e6d5595d1cfe71d9d735f012
Mo Ibrahim <106864123+mohamedibrahim5@users.noreply.github.com>
2025-05-21 02:44:25 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 if DEBUG:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "railway",              # Replace with your actual DB name
+            "USER": "postgres",             # Replace with your actual DB user
+            "PASSWORD": "cYneBTChJqLOyGzMhjvDJmyufSuAFahB",   # Replace with your actual DB password
+            "HOST": "postgres.railway.internal",  # Use Railway's host
+            "PORT": "5432",                # PostgreSQL default port
         }
     }
 else:
